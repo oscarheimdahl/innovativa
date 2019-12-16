395bf5c61f9fb62afa90117ff6faef4537cbf7ac
jonathansmedborn <jonathan.smedborn@gmail.com>
2019-12-16 17:02:08 +0100

    Roomsensor IDs aded

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './app.css';
+import { CLIENT_RENEG_LIMIT } from 'tls';
+import { jsxClosingElement } from '@babel/types';
 
 let req =
   'https://daresay-dev.eu-gb.cf.appdomain.cloud/innovativa/' +
-  'A81758FFFE03BC34/' +
+  'A81758FFFE036B8E/' +
   '2019:01:13%2000:00:00/' +
   '2019:11:15%2000:00:00/' +
   '100000000/' +
@@ -48,25 +50,26 @@ class App extends Component {
     axios.get(req).then(res => {
       let temps = [];
       let measuredDates = [];
-      res.data
-        .filter(ele => {
-          let useTemp = true;
-          let date = ele.time.slice(5, 10);
-          let hour = ele.time.slice(11, 13);
-          measuredDates.forEach(measuredDate => {
-            if (measuredDate === date) useTemp = false;
-          });
-          if (useTemp) {
-            measuredDates.push(date);
-          }
-          return useTemp;
-        })
-        .map(ele =>
-          temps.push(ele.dd.temperature + ' - ' + ele.time.slice(5, 10))
-        );
-      temps.reverse();
-      console.log(temps.length);
-      this.setState({ temps });
+      console.log(res);
+      // res.data
+      //   .filter(ele => {
+      //     let useTemp = true;
+      //     let date = ele.time.slice(5, 10);
+      //     let hour = ele.time.slice(11, 13);
+      //     measuredDates.forEach(measuredDate => {
+      //       if (measuredDate === date) useTemp = false;
+      //     });
+      //     if (useTemp) {
+      //       measuredDates.push(date);
+      //     }
+      //     return useTemp;
+      //   })
+      //   .map(ele =>
+      //     temps.push(ele.dd.temperature + ' - ' + ele.time.slice(5, 10))
+      //   );
+      // temps.reverse();
+      // console.log(temps.length);
+      // this.setState({ temps });
     });
   };
 }
---
+++ src/roomInfo.js
@@ -0,0 +1,41 @@
+rooms= {
+    NC355: {
+        CO2: "A81758FFFE03BCED",
+        Eye: "A81758FFFE03BD32",
+        Sound: "A81758FFFE",
+        EltLite: "A81758FFFE03DED4",
+        ELT2:"A81758FFFE",
+    },
+    NC356:{
+        CO2: "A81758FFFE03BCEE",
+        Eye: "A81758FFFE03BD33",
+        Sound: "A81758FFFE",
+        EltLite: "A81758FFFE03DEC5",
+        ELT2:"A81758FFFE",
+    },
+    NC357:{
+        CO2: "A81758FFFE03BCEF",
+        Eye: "A81758FFFE03BD34",
+        Eye2: "A81758FFFE03BD35",
+        Sound: "A81758FFFE",
+        EltLite: "A81758FFFE03DEC2",
+        EltLite2: "A81758FFFE03DEC2",
+        ELT2: "A81758FFFE",
+        Elpuls: "A81758FFFE036B8E",
+    },
+    NC358:{
+        CO2: "A81758FFFE03BCF1",
+        Eye: "A81758FFFE03BD37",
+        Sound: "A81758FFFE",
+        EltLite: "A81758FFFE03DEDF",
+        ELT2:"A81758FFFE",
+        Elpuls: "A81758FFFE036B38",
+    },
+    NC359:{
+        CO2: "A81758FFFE03BCF0",
+        Eye: "A81758FFFE03BD36",
+        Sound: "A81758FFFE",
+        EltLite: "A81758FFFE03DEC0",
+    },
+
+}
